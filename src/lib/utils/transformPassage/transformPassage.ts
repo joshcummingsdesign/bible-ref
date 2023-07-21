9c789909f41b6f515aecff5f105c6149d0d1cef4
Josh Cummings <joshcummingsdesign@gmail.com>
2023-07-21 06:57:42 -0700

    fix: Fix search and button style

--- src/lib/utils/transformPassage/transformPassage.ts
+++ src/lib/utils/transformPassage/transformPassage.ts
@@ -8,30 +8,33 @@ export const transformPassage = (chapterIds: string[], content: string): string 
     let prevNum = parseBookId(chapterIds[0])!.chapter;
     const sep = '<p class="p"><span data-number="';
 
-    const transformedContent = content
-        .split(sep)
-        .slice(1)
-        .reduce((acc, val, i) => {
-            const matches = val.match(/data-sid="[0-9]?[A-Z]*\s([0-9]*)/);
-            const num = matches && matches.length > 1 ? Number(matches[1]) : prevNum;
+    let paragraphs = content.split(sep);
 
-            if (i === 0) {
-                const book = parseBookId(chapterIds[index])!;
-                const text = `<h3>${book.title} ${book.chapter}</h3>${sep}${val}`;
-                index++;
-                return text;
-            }
+    if (paragraphs[0] === '') {
+        paragraphs = paragraphs.slice(1);
+    }
 
-            if (num !== prevNum) {
-                const book = parseBookId(chapterIds[index])!;
-                const text = `${acc}<h3>${book.title} ${book.chapter}</h3>${sep}${val}`;
-                index++;
-                prevNum = num;
-                return text;
-            }
+    const transformedContent = paragraphs.reduce((acc, val, i) => {
+        const matches = val.match(/data-sid="[0-9]?[A-Z]*\s([0-9]*)/);
+        const num = matches && matches.length > 1 ? Number(matches[1]) : prevNum;
 
-            return `${acc}${sep}${val}`;
-        }, '');
+        if (i === 0) {
+            const book = parseBookId(chapterIds[index])!;
+            const text = `<h3>${book.title} ${book.chapter}</h3>${sep}${val}`;
+            index++;
+            return text;
+        }
+
+        if (num !== prevNum) {
+            const book = parseBookId(chapterIds[index])!;
+            const text = `${acc}<h3>${book.title} ${book.chapter}</h3>${sep}${val}`;
+            index++;
+            prevNum = num;
+            return text;
+        }
+
+        return `${acc}${sep}${val}`;
+    }, '');
 
     return transformedContent
         .replaceAll(/<span data-number="1"[^>]*>1<\/span>/g, '')
